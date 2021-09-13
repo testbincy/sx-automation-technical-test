186babaf2f56201906c49dced235bb4db1c2dc68
Dee Lalwani <Deepplalwani@hotmail.com>
2021-09-13 12:22:56 +1200

    Removed support

--- src/support/action/clearInputField.ts
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'webdriverio';
-
-/**
- * Clear a given input field (placeholder for WDIO's clearElement)
- * @param  {String}   selector Element selector
- */
-export default (selector: Selector) => {
-    $(selector).clearValue();
-};
--- src/support/action/scroll.ts
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'webdriverio';
-
-/**
- * Scroll the page to the given element
- * @param  {String}   selector Element selector
- */
-export default (selector: Selector) => {
-    $(selector).scrollIntoView();
-};
